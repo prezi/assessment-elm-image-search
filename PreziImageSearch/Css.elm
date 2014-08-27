f7d8dce0ba2b59ad90d1ba4548100fa871c07382
Dávid István Bíró <david.biro@prezi.com>
2014-08-27 18:27:20 +0200

    format / order

--- PreziImageSearch/Css.elm
+++ PreziImageSearch/Css.elm
@@ -5,10 +5,10 @@ import Html ((:=), Attribute)
 cssClass : String -> Attribute
 cssClass shortCssClassName = "className" := ("prezi-img-search-" ++ shortCssClassName)
 
-header = cssClass "header"
-input = cssClass "input"
-widget = cssClass "widget"
-results = cssClass "results"
-resultCol = cssClass "result-col"
+header      = cssClass "header"
+input       = cssClass "input"
+resultCol   = cssClass "result-col"
 resultEntry = cssClass "result-entry"
-submit = cssClass "submit"
+results     = cssClass "results"
+submit      = cssClass "submit"
+widget      = cssClass "widget"
