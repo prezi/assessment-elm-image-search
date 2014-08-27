4a758fedf1f9126bc73b9a6ddf9041fd0a4c8b4b
Dávid István Bíró <david.biro@prezi.com>
2014-08-27 12:25:17 +0200

    create new module that will be embedded

---
+++ PreziImageSearchEmbed.elm
@@ -0,0 +1,19 @@
+module PreziImageSearchEmbed where
+
+import PreziImageSearch
+import PreziImageSearch.Config as Config
+import PreziImageSearch.Config (..)
+
+config : Config.Config
+config =
+    { width                = 300
+    , imagePadding         = 3
+    , googleCustomSearchId = ""
+    , googleApiKey         = ""
+    , googleTestResponse   = False
+    , language             = Config.Hun
+    }
+
+main : Signal Element
+main = PreziImageSearch.widget config
+
