6efd3db07abb2a6af63f8adfd47b4c7ee7761662
Dávid István Bíró <david.biro@prezi.com>
2014-09-23 20:27:26 +0200

    Updated main files to the new style guide

--- PreziImageSearchDemo.elm
+++ PreziImageSearchDemo.elm
@@ -5,15 +5,14 @@ import PreziImageSearch.Config as Config
 import PreziImageSearch.Config (..)
 
 config : Config.Config
-config =
-    { width                = 300
-    , imagePadding         = 3
-    , googleCustomSearchId = ""
-    , googleApiKey         = ""
-    , googleTestResponse   = False
-    , language             = Config.Hun
+config = { 
+        width = 300,
+        imagePadding = 3,
+        googleCustomSearchId = "",
+        googleApiKey  = "",
+        googleTestResponse = False,
+        language = Config.Hun
     }
 
 main : Signal Element
 main = PreziImageSearch.widget (constant config)
-
--- PreziImageSearchEmbed.elm
+++ PreziImageSearchEmbed.elm
@@ -11,13 +11,13 @@ import PreziImageSearch.SearchEngine (SearchResultEntry)
 
 {- Input ports -}
 
-port configIn : Signal
-    { width                : Int
-    , imagePadding         : Int
-    , googleCustomSearchId : String
-    , googleApiKey         : String
-    , googleTestResponse   : Bool
-    , language             : String
+port configIn : Signal {
+        width : Int,
+        imagePadding : Int,
+        googleCustomSearchId : String,
+        googleApiKey : String,
+        googleTestResponse : Bool,
+        language : String
     }
 
 {- Output ports -}
@@ -34,24 +34,27 @@ convertResult entry =
         intProp name value = (name, Json.Number (toFloat value))
     in
         if not (String.isEmpty entry.url)
-            then Just
-                    (Json.Object
-                        (Dict.fromList
-                            [ strProp "url"             entry.url
-                            , intProp "width"           entry.width
-                            , intProp "height"          entry.height
-                            , strProp "thumbnailUrl"    entry.thumbnailUrl
-                            , intProp "thumbnailWidth"  entry.thumbnailWidth
-                            , intProp "thumbnailHeight" entry.thumbnailHeight
-                            ]))
+            then Just (
+                    Json.Object (
+                        Dict.fromList [
+                            strProp "url" entry.url,
+                            intProp "width" entry.width,
+                            intProp "height" entry.height,
+                            strProp "thumbnailUrl" entry.thumbnailUrl,
+                            intProp "thumbnailWidth"  entry.thumbnailWidth,
+                            intProp "thumbnailHeight" entry.thumbnailHeight
+                        ]
+                    )
+                )
             else Nothing
 
 {- Config conversion -}
 
 convertedConfigSignal = convertConfig <~ configIn
 
-convertConfig jsConf =
-    { jsConf | language <-
+convertConfig jsConf = {
+        jsConf |
+            language <-
                 case jsConf.language of
                     "Hun" -> Config.Hun
                     "Eng" -> Config.Eng
